5c19cf1fc90cab87303db7c3a9a4899cc1bce9aa
Meghna Prashar <meghnaprashar@Meghnas-MacBook-Pro.local>
2019-05-16 16:19:31 +0530

    adding changes

---
+++ src/index.js
@@ -0,0 +1,181 @@
+const fetch = require('isomorphic-fetch')
+const Rx = require('rx')
+const url = require('url')
+
+class OothClient {
+    constructor({
+        oothUrl,
+        standalone,
+        apiLoginUrl,
+        apiLogoutUrl
+    }) {
+        this.oothUrl = oothUrl
+        this.standalone = standalone
+        if (standalone) {
+            this.apiLoginUrl = apiLoginUrl
+            this.apiLogoutUrl = apiLogoutUrl
+        }
+    }
+    start() {
+        return new Promise((resolve, reject) => {
+            if (!this.started) {
+                this.started = true
+                this.user()
+                this.subscribeStatus()
+                return this.status().then(resolve)
+            } else {
+                return resolve(this.user().getValue())
+            }
+        })
+    }
+    user() {
+        if (!this.userSubject) {
+            this.userSubject = new Rx.BehaviorSubject(null)
+        }
+        return this.userSubject
+    }
+    next(user) {
+        if (this.userSubject) {
+            this.userSubject.onNext(user)
+        }
+        return user
+    }
+    authenticate(strategy, method, body) {
+        return fetch(`${this.oothUrl}/${strategy}/${method}`, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: body && JSON.stringify(body),
+                credentials: 'include'
+            })
+            .then((response) => {
+                return response.json()
+            })
+            .then((response) => {
+                if (response.status === 'error') {
+                    throw new Error(response.message)
+                }
+                const { user, token } = response
+                if (this.standalone) {
+                    return fetch(this.apiLoginUrl, {
+                        method: 'POST',
+                        headers: {
+                            'Authorization': `JWT ${token}`
+                        },
+                        credentials: 'include'
+                    }).then(() => {
+                        return user
+                    })
+                } else {
+                    return user
+                }
+            })
+            .then((user) => {
+                return this.next(user)
+            })
+    }
+    method(strategy, method, body) {
+        return fetch(`${this.oothUrl}/${strategy}/${method}`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(body),
+            credentials: 'include'
+        }).then(response => {
+            return response.json()
+        }).then(response => {
+            if (response.status === 'error') {
+                throw new Error(response.message)
+            } else {
+                if (response.user) {
+                    this.next(response.user)
+                }
+                return response
+            }
+        })
+    }
+    logout() {
+        return fetch(`${this.oothUrl}/logout`, {
+            method: 'POST',
+            credentials: 'include'
+        }).then(response => {
+            if (this.standalone) {
+                return fetch(this.apiLogoutUrl, {
+                    method: 'POST',
+                    credentials: 'include'
+                })
+            }
+        }).then(() => {
+            return this.next(null)
+        })
+    }
+    status(cookies) {
+        const opts = {
+            method: 'GET',
+        }
+        if (cookies) {
+            opts.headers = {
+                'Cookie': Object.keys(cookies).map(key => `${key}=${cookies[key]}`).join('; ')
+            }
+        } else {
+            opts.credentials = 'include'
+        }
+        return fetch(`${this.oothUrl}/status`, opts).then(response => {
+            return response.json()
+        }).then(({ user }) => {
+            return this.next(user)
+        })
+    }
+    subscribeStatus() {
+        const HEARTBEAT = {
+            ping: 'PING',
+            pong: 'PONG',
+            timeout: 31000
+        }
+        let
+            pongTimeout,
+            heartBeat = ws => {
+                console.log("Got Ping >>> sending Pong")
+                ws.send(JSON.stringify({ msg: HEARTBEAT.pong }))
+                // prevent the socket connection close
+                clearTimeout(pongTimeout)
+                // schedule socket connection close
+                pongTimeout = setTimeout(function heartBeatCheck() {
+                    console.log("Socket Closing from client")
+                    // close the socket connection
+                    ws.close()
+                }, HEARTBEAT.timeout)
+            },
+            stopHeartBeat = () => {
+                clearTimeout(pongTimeout)
+            };
+        if (typeof WebSocket !== 'undefined') {
+            const urlParts = url.parse(this.oothUrl)
+            const protocol = urlParts.protocol === 'https:' ? 'wss' : 'ws'
+            const wsUrl = `${protocol}://${urlParts.host}${urlParts.path}/status`
+            const socket = new WebSocket(wsUrl)
+            socket.onerror = (err) => {
+                console.error(err)
+                stopHeartBeat()
+            }
+            socket.onopen = () => {}
+            socket.onclose = () => {
+                stopHeartBeat()
+            }
+            socket.onmessage = ({ data }) => {
+                var { user, msg } = JSON.parse(data)
+                user = user || null
+                    if (msg && msg === HEARTBEAT.ping) {
+                        // resetHeartBeat()
+                        heartBeat(socket)
+                        return
+                    }
+                return this.next(user)
+            }
+        }
+    }
+}
+
+module.exports = OothClient
